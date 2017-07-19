43f20095c54e1cd11b02b6f04bb8025271a0627c
Alexey Peshin <alexey.peshin@corptech.lv>
2017-07-19 19:33:33 +0300

    Fixing PDF display and Print issue

--- assets/web/viewer.js
+++ assets/web/viewer.js
@@ -5617,7 +5617,7 @@ var pdfjsWebLibs;
    var DISABLE_AUTO_FETCH_LOADING_BAR_TIMEOUT = 5000;
    function configure(PDFJS) {
     PDFJS.imageResourcesPath = './images/';
-    PDFJS.workerSrc = '../build/pdf.worker.js';
+    //PDFJS.workerSrc = '../build/pdf.worker.js';
     PDFJS.cMapUrl = '../web/cmaps/';
     PDFJS.cMapPacked = true;
    }
